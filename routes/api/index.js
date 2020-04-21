6a7617337440919c43d4a0f2aeebf80965562517
Cat Kretzschmar <cat.ktz@gmail.com>
2020-04-21 18:04:04 -0400

    moved a line

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,8 +5,8 @@ const path = require("path");
 // Book routes
 router.use("/books", bookRoutes);
 
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });   
 }
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/savedBooks", {
