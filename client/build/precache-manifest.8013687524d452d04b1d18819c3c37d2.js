a880ef3e4ec4a5276102542d0527bb4daf802ab5
Cat Kretzschmar <cat.ktz@gmail.com>
2020-04-21 16:19:23 -0400

    change to index

---
+++ client/build/precache-manifest.8013687524d452d04b1d18819c3c37d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af627d0d9635bc8de9ec",
+    "url": "/static/css/main.dd4f68d5.chunk.css"
+  },
+  {
+    "revision": "af627d0d9635bc8de9ec",
+    "url": "/static/js/main.5e9f225e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f620896c0ad944689a9",
+    "url": "/static/js/2.af232ad2.chunk.js"
+  },
+  {
+    "revision": "4f6fc7d53bfdf74d7d43ca4f5e3fcab5",
+    "url": "/index.html"
+  }
+];
