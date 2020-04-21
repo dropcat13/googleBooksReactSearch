d7670ca061b964186ef442f92eb68a701de21406
Cat Kretzschmar <cat.ktz@gmail.com>
2020-04-21 18:14:51 -0400

    moved a line

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const routes = require("./routes")
 var mongoose = require("mongoose");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });   
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });   
 }
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/savedBooks", {
   useCreateIndex: true,
